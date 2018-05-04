679dcf38d6f157b27bfc147f0dc2838009be302e
weiguo xia <xiaw@miamioh.edu>
2018-05-03 22:35:46 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute("alt", "smaller image of the restaurant");
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
